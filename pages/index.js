e1de70431ddc27b325f09630a6c7e8890b313895
Wesley Rast <wesley.rast@paya.com>
2019-01-24 16:53:48 -0500

    Add home blog layout with PostLink components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Layout from '../components/MyLayout';
+import Link from 'next/link';
+
+const PostLink = ({ title = 'no title' }) => (
+  <li>
+    <Link href={`/post?title=${title}`}>
+      <a>{title}</a>
+    </Link>
+  </li>
+);
+PostLink.propTypes = {
+  title: PropTypes.string.isRequired
+};
 
 export default () => (
   <Layout>
-    <p>Hello Next.js</p>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js" />
+      <PostLink title="Learn Next.js is awesome" />
+      <PostLink title="Deploy apps with Zeit" />
+    </ul>
   </Layout>
 );
