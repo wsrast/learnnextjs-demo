78d94a0c0fda00083bab0a020b60a459503dc62b
Wesley Rast <wesley.rast@paya.com>
2019-02-04 16:22:02 -0500

    Format post.js

--- pages/post.js
+++ pages/post.js
@@ -4,16 +4,19 @@ import { withRouter } from 'next/router';
 import Layout from '../components/MyLayout';
 
 const Content = withRouter(({ router }) => (
-  <div>
-    <h1>{router.query.title}</h1>
-    <p>This is the blog post content</p>
-  </div>
+	<div>
+		<h1>{router.query.title}</h1>
+		<p>This is the blog post content</p>
+	</div>
 ));
+Content.propTypes = {
+	router: PropTypes.objectOf(PropTypes.object)
+};
 
 const Page = () => (
-  <Layout>
-    <Content />
-  </Layout>
+	<Layout>
+		<Content />
+	</Layout>
 );
 
 export default Page;
