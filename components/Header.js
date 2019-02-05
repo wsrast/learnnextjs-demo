6b38d6a0ad91154ac6816d550645a9d80f946cd3
Wesley Rast <wesley.rast@paya.com>
2019-02-05 10:14:20 -0500

    Add async-reactor dependency. Add test async component fetch to Header.

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+import { asyncReactor } from 'async-reactor';
 
 const linkStyle = {
 	marginRight: 15
 };
 
-const Header = () => (
-	<div>
-		<Link href="/">
-			<a style={linkStyle}>Home</a>
-		</Link>
-		<Link href="/about" prefetch>
-			<a style={linkStyle}>About</a>
-		</Link>
-	</div>
-);
+const Header = async () => {
+	const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+	const posts = await data.json();
 
-export default Header;
+	console.log(`posts: `, posts);
+
+	return (
+		<div>
+			<Link href="/">
+				<a style={linkStyle}>Home</a>
+			</Link>
+			<Link href="/about" prefetch>
+				<a style={linkStyle}>About</a>
+			</Link>
+		</div>
+	);
+};
+
+const Load = () => <h2>Loading...</h2>;
+
+const Error = () => <h2>Error</h2>;
+
+export default asyncReactor(Header, Load, Error);
