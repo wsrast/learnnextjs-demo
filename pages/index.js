c65500028467caddc475e93fd93484e32fd484c3
Wesley Rast <wesley.rast@paya.com>
2019-02-05 08:49:20 -0500

    Add isomorphic-unfetch dependency. Add API call to index page.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/MyLayout';
 
+const Index = ({ shows }) => (
+	<Layout>
+		<h1>Batman TV Shows</h1>
+		<ul>
+			{shows.map(({ show }) => (
+				<li key={`show${show.id}`}>
+					<Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+						<a>{show.name}</a>
+					</Link>
+				</li>
+			))}
+		</ul>
+	</Layout>
+);
+Index.propTypes = {
+	shows: PropTypes.arrayOf(PropTypes.object)
+};
+Index.defaultProps = {
+	shows: []
+};
+Index.getInitialProps = async function() {
+	const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+	const data = await res.json();
+
+	console.log(`Show data fetched. Count: ${data.length}`);
+	return {
+		shows: data
+	};
+};
+
+export default Index;
+
+/*
 const PostLink = ({ id = '', title = 'no title' }) => (
 	<li>
 		<Link as={`/p/${id}`} href={`/post?title=${title}`}>
@@ -25,3 +59,4 @@ export default () => (
 		</ul>
 	</Layout>
 );
+*/
