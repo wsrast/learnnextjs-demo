e9a5feed786274a85ec076d86e6c79c40ad903c1
Wesley Rast <wesley.rast@paya.com>
2019-02-13 11:45:46 -0500

    Add fetch to post page

--- pages/index.js
+++ pages/index.js
@@ -35,28 +35,3 @@ Index.getInitialProps = async function() {
 };
 
 export default Index;
-
-/*
-const PostLink = ({ id = '', title = 'no title' }) => (
-	<li>
-		<Link as={`/p/${id}`} href={`/post?title=${title}`}>
-			<a>{title}</a>
-		</Link>
-	</li>
-);
-PostLink.propTypes = {
-	id: PropTypes.string.isRequired,
-	title: PropTypes.string.isRequired
-};
-
-export default () => (
-	<Layout>
-		<h1>My Blog</h1>
-		<ul>
-			<PostLink id="hello-nextjs" title="Hello Next.js" />
-			<PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-			<PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
-		</ul>
-	</Layout>
-);
-*/
--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'next/router';
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/MyLayout';
 
+const Post = ({
+	show: {
+		name,
+		summary,
+		image: { medium }
+	}
+}) => (
+	<Layout>
+		<h1>{name}</h1>
+		<p>{summary.replace(/<[/]?p>/g, '')}</p>
+		<img src={medium} alt={name} />
+	</Layout>
+);
+Post.propTypes = {
+	show: PropTypes.objectOf(PropTypes.any).isRequired
+};
+Post.getInitialProps = async function({ query }) {
+	const { id } = query;
+	const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+	const show = await res.json();
+
+	// console.log(`context: `, arguments[0]);
+	console.log(`Fetched show: ${show.name}`);
+
+	return { show };
+};
+
+/*
 const Content = withRouter(({ router }) => (
 	<div>
 		<h1>{router.query.title}</h1>
@@ -18,5 +46,6 @@ const Page = () => (
 		<Content />
 	</Layout>
 );
+*/
 
-export default Page;
+export default Post;
